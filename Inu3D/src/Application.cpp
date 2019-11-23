f8173cc7d074219dac13d88dd1a5695dc768114a
Mikel <mikeliromaka@gmail.com>
2019-11-23 02:11:35 +0100

    feat: Changed rendered  model to a cube

--- Inu3D/src/Application.cpp
+++ Inu3D/src/Application.cpp
@@ -14,32 +14,87 @@ int main(void)
 	Renderer renderer(&shader);
 
 	vector<float> vertices = {
-		-0.5f,  0.5f, 0, // v0
-		-0.5f, -0.5f, 0, // v1
-		 0.5f, -0.5f, 0, // v2
-		 0.5f,  0.5f, 0, // v3
+		-0.5f,  0.5f, -0.5f,
+		-0.5f, -0.5f, -0.5f,
+		 0.5f, -0.5f, -0.5f,
+		 0.5f,  0.5f, -0.5f,
+
+		-0.5f,  0.5f,  0.5f,
+		-0.5f, -0.5f,  0.5f,
+		 0.5f, -0.5f,  0.5f,
+		 0.5f,  0.5f,  0.5f,
+
+		 0.5f,  0.5f, -0.5f,
+		 0.5f, -0.5f, -0.5f,
+		 0.5f, -0.5f,  0.5f,
+		 0.5f,  0.5f,  0.5f,
+
+		-0.5f,  0.5f, -0.5f,
+		-0.5f, -0.5f, -0.5f,
+		-0.5f, -0.5f,  0.5f,
+		-0.5f,  0.5f,  0.5f,
+
+		-0.5f,  0.5f,  0.5f,
+		-0.5f,  0.5f, -0.5f,
+		 0.5f,  0.5f, -0.5f,
+		 0.5f,  0.5f,  0.5f,
+
+		-0.5f, -0.5f,  0.5f,
+		-0.5f, -0.5f, -0.5f,
+		 0.5f, -0.5f, -0.5f,
+		 0.5f, -0.5f,  0.5f
 	};
 
 	vector<unsigned int> indices = {
-		0, 1, 3, 
-		3, 1, 2
+		 0,  1,  3,
+		 3,  1,  2,
+		 4,  5,  7,
+		 7,  5,  6,
+		 8,  9, 11,
+		11,  9, 10,
+		12, 13, 15,
+		15, 13, 14,
+		16, 17, 19,
+		19, 17, 18,
+		20, 21, 23,
+		23, 21, 22
 	};
 
 	vector<float> texture_coords = {
-		0, 0, // v0
-		0, 1, // v1
-		1, 1, // v2
-		1, 0  // v3
+		0, 0,
+		0, 1,
+		1, 1,
+		1, 0,
+		0, 0,
+		0, 1,
+		1, 1,
+		1, 0,
+		0, 0,
+		0, 1,
+		1, 1,
+		1, 0,
+		0, 0,
+		0, 1,
+		1, 1,
+		1, 0,
+		0, 0,
+		0, 1,
+		1, 1,
+		1, 0,
+		0, 0,
+		0, 1,
+		1, 1,
+		1, 0
 	};
 
 	RawModel *model = loader.load_to_vao(vertices, texture_coords, indices);
 	Texture *texture = loader.load_texture("res/textures/pepe.png");
 	TexturedModel *textured_model = new TexturedModel(model, texture);
-	Entity *entity = new Entity(textured_model, glm::vec3(0, 0, -1), 0.0, 0.0, 0.0, 1.0);
+	Entity *entity = new Entity(textured_model, glm::vec3(0, 0, -5), 0.0, 0.0, 0.0, 1.0);
 
 	while (!window.is_running())
 	{
-		entity->increase_position(0, 0, -0.01f);
+		entity->increase_rotation(1, 1, 0);
 		window.clear(0.1f, 0.2f, 0.3f);
 		shader.start();
 		shader.load_view_matrix(window.get_camera());
--- Inu3D/src/renderer/Window.cpp
+++ Inu3D/src/renderer/Window.cpp
@@ -60,8 +60,9 @@ Window::~Window()
 
 void Window::clear(float x, float y, float z)
 {
+	GLCall(glEnable(GL_DEPTH_TEST));
 	GLCall(glClearColor(x, y, z, 1.0f));
-	GLCall(glClear(GL_COLOR_BUFFER_BIT));
+	GLCall(glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT));
 }
 
 void Window::update()
